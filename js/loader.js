194e59f00240c24e1c961ef9fa0ce6411d93a326
dohyeon-Lee <dohyeon@postech.ac.kr>
2026-05-26 20:12:50 +0900

    renewer

---
+++ js/loader.js
@@ -0,0 +1,26 @@
+const PUBLICATIONS = [
+  'publications/ral2025/content.html',
+  'publications/iros2023/content.html',
+];
+
+const PROJECTS = [
+  // 'projects/reachability_mppi/content.html',
+  'projects/fsdrone_dev/content.html',
+  'projects/pirnn/content.html',
+  'projects/quad_mani/content.html',
+  'projects/glidable_fsdrone/content.html',
+  'projects/quad_wheel/content.html',
+  'projects/astar_nav/content.html',
+];
+
+async function loadSection(tbodyId, files) {
+  const tbody = document.getElementById(tbodyId);
+  for (const file of files) {
+    const res = await fetch(file);
+    const html = await res.text();
+    tbody.insertAdjacentHTML('beforeend', html);
+  }
+}
+
+loadSection('publications-body', PUBLICATIONS);
+loadSection('projects-body', PROJECTS);
